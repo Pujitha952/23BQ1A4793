fdf972c3870ce4e0106b4504e332d75d00c0cbf9
Pujitha952 <thalluripujitha@gmail.com>
2026-06-05 11:46:06 +0530

    Updated logger middleware

--- question1/logger.js
+++ question1/logger.js
@@ -1,20 +1,26 @@
+const token = process.env.TOKEN;
+
 async function Log(stack, level, pkg, message) {
-  await fetch(
-    "http://4.224.186.213/evaluation-service/logs",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer YOUR_ACCESS_TOKEN"
-      },
-      body: JSON.stringify({
-        stack,
-        level,
-        package: pkg,
-        message
-      })
-    }
-  );
+  try {
+    await fetch(
+      "http://4.224.186.213/evaluation-service/logs",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          stack,
+          level,
+          package: pkg,
+          message
+        })
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = Log;
